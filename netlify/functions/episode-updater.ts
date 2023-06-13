76af526d7f2b71f732d4b7026b243a8efab22e86
Matthew Saxe <msaxe@perryweather.com>
2023-06-13 09:10:02 -0500

    Update process to use github api

--- netlify/functions/episode-updater.ts
+++ netlify/functions/episode-updater.ts
@@ -4,131 +4,165 @@ import axios from 'axios';
 import { saveSpotifyEpisodes, saveYoutubeEpisodes } from '../../helpers/data';
 import fs from 'fs';
 import crypto from 'crypto';
-import { simpleGit, CleanOptions } from 'simple-git';
+import { Octokit } from '@octokit/core';
 
-simpleGit().clean(CleanOptions.FORCE);
-const { YOUTUBE_API_BASE_URL, YOUTUBE_API_KEY, YOUTUBE_PLAYLIST_ID, SPOTIFY_API_BASE_URL, SPOTIFY_API_AUTH_BASE_URL, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_SHOW_ID, BUILD_HOOK_URL } = process.env;
+const {
+  YOUTUBE_API_BASE_URL,
+  YOUTUBE_API_KEY,
+  YOUTUBE_PLAYLIST_ID,
+  SPOTIFY_API_BASE_URL,
+  SPOTIFY_API_AUTH_BASE_URL,
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_SHOW_ID,
+  GITHUB_API_TOKEN,
+  GITHUB_OWNER,
+  GITHUB_REPO,
+  GITHUB_NAME,
+  GITHUB_EMAIL,
+} = process.env;
+const octokit = new Octokit({ auth: GITHUB_API_TOKEN });
 
 const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-    if (!fs.existsSync('./data')) {
-        fs.mkdirSync('./data');
+  if (!fs.existsSync('./data')) {
+    fs.mkdirSync('./data');
+  }
+
+  const spotifyEpisodes = await getSpotifyEpisodes();
+  const youtubeEpisodes = await getYoutubeEpisodes();
+
+  const newSpotifyData = saveSpotifyEpisodes(spotifyEpisodes, './data/spotify-episodes.json');
+  const newYoutubeData = saveYoutubeEpisodes(youtubeEpisodes, './data/youtube-videos.json');
+
+  if (newSpotifyData || newYoutubeData) {
+    console.log('New data found, triggering build');
+  } else {
+    console.log('Data is all up-to-date, skipping rebuild');
+    return { statusCode: 200 };
+  }
+
+  // Commit to repo
+  try {
+    const hash = crypto.randomBytes(10).toString('hex');
+    const files = [
+      { path: 'data/spotify-episodes.json', data: spotifyEpisodes },
+      { path: 'data/youtube-videos.json', data: youtubeEpisodes },
+    ];
+
+    for (const file of files) {
+      const {
+        data: { sha },
+      } = await octokit.request(`GET /repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${file.path}`, {
+        owner: GITHUB_OWNER,
+        repo: GITHUB_REPO,
+        file_path: file.path,
+      });
+
+      await octokit.request(`PUT /repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${file.path}`, {
+        owner: GITHUB_OWNER as string,
+        repo: GITHUB_REPO as string,
+        path: file.path,
+        message: `Update episode data. Hash: #${hash}`,
+        comitter: {
+          name: GITHUB_NAME as string,
+          email: GITHUB_EMAIL as string,
+        },
+        content: Buffer.from(JSON.stringify(file.data)).toString('base64'),
+        sha,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      });
     }
+    
+    return { statusCode: 200 };
+  } catch (err) {
+    const message = `Error in git process. [ERROR] ${err}`;
 
-    const spotifyEpisodes = await getSpotifyEpisodes();
-    const youtubeEpisodes = await getYoutubeEpisodes();
-
-    const newSpotifyData = saveSpotifyEpisodes(spotifyEpisodes, './data/spotify-episodes.json');
-    const newYoutubeData = saveYoutubeEpisodes(youtubeEpisodes, './data/youtube-videos.json');
-
-    if (newSpotifyData || newYoutubeData) {
-        console.log('New data found, triggering build');
-    } else {
-        console.log('Data is all up-to-date, skipping rebuild');
-        return { statusCode: 200 };
-    }
-
-    // Commit to repo
-    try {
-        const git = simpleGit();
-        const hash = crypto.randomBytes(10).toString('hex');
-
-        console.log('Adding data files.');
-        await git.add('.');
-
-        console.log(`Committing files. Hash: #${hash}`);
-        await git.commit(`Update data. Hash: ${hash}`);
-
-        console.log('Pushing commit.');
-        git.push('origin', 'master');
-
-        return { statusCode: 200 };
-    } catch (err) {
-        const message = `Error in git process. [ERROR] ${err}`;
-
-        console.error(message);
-        return { statusCode: 500, message };
-    }
+    console.error(message);
+    return { statusCode: 500, message };
+  }
 };
 
 async function getSpotifyToken() {
-    try {
-        const basic = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
-        const form = new URLSearchParams();
+  try {
+    const basic = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
+    const form = new URLSearchParams();
 
-        form.append('grant_type', 'client_credentials');
+    form.append('grant_type', 'client_credentials');
 
-        const res = await axios.post(`${SPOTIFY_API_AUTH_BASE_URL}/token`, form, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: `Basic ${basic}`,
-            },
-        });
+    const res = await axios.post(`${SPOTIFY_API_AUTH_BASE_URL}/token`, form, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${basic}`,
+      },
+    });
 
-        return res.data.access_token;
-    } catch (err) {
-        logAxiosError(err);
-    }
+    return res.data.access_token;
+  } catch (err) {
+    logAxiosError(err);
+  }
 }
 
 async function getSpotifyEpisodes(offset: number = 0, pageSize: number = 50): Promise<Array<any>> {
-    let episodes = [];
+  let episodes = [];
 
-    try {
-        const res = await axios.get(`${SPOTIFY_API_BASE_URL}/shows/${SPOTIFY_SHOW_ID}/episodes`, {
-            headers: { Authorization: `Bearer ${await getSpotifyToken()}` },
-            params: {
-                limit: pageSize,
-                market: 'ES',
-                offset,
-            },
-        });
+  try {
+    const res = await axios.get(`${SPOTIFY_API_BASE_URL}/shows/${SPOTIFY_SHOW_ID}/episodes`, {
+      headers: { Authorization: `Bearer ${await getSpotifyToken()}` },
+      params: {
+        limit: pageSize,
+        market: 'ES',
+        offset,
+      },
+    });
 
-        episodes = [...res.data.items];
+    episodes = [...res.data.items];
 
-        if (!!res.data.next) episodes = episodes.concat(await getSpotifyEpisodes(offset + pageSize));
-    } catch (err) {
-        logAxiosError(err);
-    }
+    if (!!res.data.next) episodes = episodes.concat(await getSpotifyEpisodes(offset + pageSize));
+  } catch (err) {
+    logAxiosError(err);
+  }
 
-    return episodes;
+  return episodes;
 }
 
 async function getYoutubeEpisodes(pageToken: string = '', pageSize: number = 50): Promise<Array<any>> {
-    let episodes = [];
+  let episodes = [];
 
-    try {
-        const res = await axios.get(`${YOUTUBE_API_BASE_URL}/playlistItems`, {
-            params: {
-                key: YOUTUBE_API_KEY,
-                playlistId: YOUTUBE_PLAYLIST_ID,
-                part: 'snippet,id',
-                maxResults: pageSize,
-                type: 'video',
-                pageToken,
-            },
-        });
+  try {
+    const res = await axios.get(`${YOUTUBE_API_BASE_URL}/playlistItems`, {
+      params: {
+        key: YOUTUBE_API_KEY,
+        playlistId: YOUTUBE_PLAYLIST_ID,
+        part: 'snippet,id',
+        maxResults: pageSize,
+        type: 'video',
+        pageToken,
+      },
+    });
 
-        episodes = [...res.data.items];
+    episodes = [...res.data.items];
 
-        if (!!res.data.nextPageToken) episodes = episodes.concat(await getYoutubeEpisodes(res.data.nextPageToken));
-    } catch (err) {
-        logAxiosError(err);
-    }
+    if (!!res.data.nextPageToken) episodes = episodes.concat(await getYoutubeEpisodes(res.data.nextPageToken));
+  } catch (err) {
+    logAxiosError(err);
+  }
 
-    return episodes;
+  return episodes;
 }
 
 function logAxiosError(axiosError: any) {
-    if (axiosError.response) {
-        console.error(axiosError.response.data);
-        console.error(axiosError.response.status);
-        console.error(axiosError.response.headers);
-    } else if (axiosError.request) {
-        console.error(axiosError.request);
-    } else {
-        console.error('Error', axiosError.message);
-    }
-    console.error(axiosError.config);
+  if (axiosError.response) {
+    console.error(axiosError.response.data);
+    console.error(axiosError.response.status);
+    console.error(axiosError.response.headers);
+  } else if (axiosError.request) {
+    console.error(axiosError.request);
+  } else {
+    console.error('Error', axiosError.message);
+  }
+  console.error(axiosError.config);
 }
 
 const handler = schedule('@daily', myHandler);
